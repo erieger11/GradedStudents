04540e4621f1057a0cd8567f8e0ff08abf5eee83
erieger11 <erieger530@gmail.com>
2024-03-20 19:03:59 -0400

    did the first two steps

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,20 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+    String firstName;
+    String lastName;
+    ArrayList<Double> examScores = new ArrayList<>();
+
+
+    public Student (String firstName, String lastName, ArrayList examScores){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = examScores;
+    }
+
+
+
+
 }
